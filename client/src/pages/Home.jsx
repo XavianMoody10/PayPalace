728e249ea9fe0b633ffdb4fe04a17b5753a1732e
Xavian Moody <XavianMoody1025@gmail.com>
2025-03-03 18:31:47 -0500

    Added animation to hero section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import HeroImage from "../assets/hero-image.png";
+import React, { useRef } from "react";
 import { Header } from "../layouts/Header";
 import { PrimaryButton } from "../components/PrimaryButton/PrimaryButton";
 import { MarqueeSlider } from "../components/MarqueeSlider/MarqueeSlider";
+import { motion, useInView } from "motion/react";
+import HeroImage from "../assets/hero-image.png";
 import paymentImage1 from "../assets/payment-image-1.png";
 import paymentImage2 from "../assets/payment-image-2.png";
 import paymentIcon1 from "../assets/payment-icon-1.svg";
@@ -10,30 +11,55 @@ import paymentIcon2 from "../assets/payment-icon-2.svg";
 import paymentIcon3 from "../assets/payment-icon-3.svg";
 
 export const Home = () => {
+  const paymentSectionRef = useRef(null);
+  const isInView = useInView(paymentSectionRef, { amount: 0.7 });
+
   return (
     <>
       <Header />
 
       <main className=" min-h-screen bg-[#E7F0FF]">
         <section className=" pt-14 w-[90%] max-w-[1400px] mx-auto flex flex-col gap-7">
-          <h1 className=" font-semibold text-4xl sm:text-5xl sm:leading-14 lg:text-7xl lg:leading-20">
+          <motion.h1
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            className=" font-semibold text-4xl sm:text-5xl sm:leading-14 lg:text-7xl lg:leading-20"
+          >
             An Entirely New Visual Experience For Wireless POS Devices
-          </h1>
+          </motion.h1>
 
-          <div className=" flex flex-col justify-between items-start gap-2 sm:flex-row sm:items-center">
+          <motion.div
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.5, ease: "easeInOut" }}
+            className=" flex flex-col justify-between items-start gap-2 sm:flex-row sm:items-center"
+          >
             <PrimaryButton>Contact Us</PrimaryButton>
 
             <p className=" font-primary sm:text-end sm:w-[68%] min-[800px]:text-xl">
               Our scalable payment processing solutions help you work better
             </p>
-          </div>
+          </motion.div>
 
-          <img src={HeroImage} alt="Person holding a tablet device" />
+          <motion.img
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 1, ease: "easeInOut" }}
+            src={HeroImage}
+            alt="Person holding a tablet device"
+          />
         </section>
 
         <MarqueeSlider />
 
-        <section className=" w-[90%] max-w-[1400px] mx-auto">
+        <motion.section
+          ref={paymentSectionRef}
+          className=" w-[90%] max-w-[1400px] mx-auto"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: isInView ? 1 : 0 }}
+          transition={{ duration: 1.2 }}
+        >
           <div className=" bg-white pt-14 pb-20 px-3 rounded-4xl sm:px-10 min-[800px]:px-16">
             <h2 className=" font-semibold text-4xl text-center lg:text-5xl lg:leading-16">
               Work faster and smarter with our scalable payment
@@ -102,7 +128,7 @@ export const Home = () => {
               </div>
             </div>
           </div>
-        </section>
+        </motion.section>
       </main>
     </>
   );
